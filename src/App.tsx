c0855bd37258c4672a7b59106d3ca82bd2846735
Daisuke Yamamura <mucho613@gmail.com>
2021-09-11 22:54:21 +0900

    add controlling by gamepad stick

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import * as THREE from "three";
+import { Canvas, useFrame } from '@react-three/fiber';
+import './css/App.css';
+
+const Thing = () => {
+  const ref = useRef<THREE.Mesh>(null!);
+  useFrame(() => {
+    const gamepads = navigator.getGamepads()
+    const gamepad = gamepads[0];
+
+    if(gamepad) {
+      ref.current.position.y -= gamepad?.axes[0] / 10;
+      ref.current.position.x -= gamepad?.axes[1] / 10;
+    }
+  });
+  return (
+    <mesh
+      ref={ref}
+      onClick={e => console.log('click')}
+      onPointerOver={e => console.log('hover')}
+      onPointerOut={e => console.log('unhover')}
+    >
+      <planeBufferGeometry attach='geometry' args={[1, 1]} />
+      <meshBasicMaterial
+        attach='material'
+        color='hotpink'
+        opacity={0.5}
+        transparent
+      />
+    </mesh>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pi-Controller</h1>
-    </div>
+    <Canvas>
+      <Thing />
+    </Canvas>
   );
 }
 
